954925f62f7aadc1ef8efa9cbddb9977a6dee0e2
EBLkinz <unconfigured@null.spigotmc.org>
2018-05-17 23:13:34 -0500

    Added the beginning of the Test class.

--- src/main/java/io/github/eblkinz/arenapvp/Main.java
+++ src/main/java/io/github/eblkinz/arenapvp/Main.java
@@ -1,5 +1,8 @@
 package io.github.eblkinz.arenapvp;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin
@@ -7,12 +10,37 @@ public class Main extends JavaPlugin
 	@Override
 	public void onEnable()
 	{
-		getLogger().info("Enabled");
+		//registerEvents(this, new );
 	}
 	
 	@Override
 	public void onDisable()
 	{
-		getLogger().info("Disabled");
+		
+	}
+	
+	/**
+	 * Gets the ArenaPvp plugin.
+	 * @return The ArenaPvp plugin.
+	 */
+	
+	public static Plugin getPlugin()
+	{
+		return Bukkit.getServer().getPluginManager().getPlugin("ArenaPvp");
+	}
+	
+	/**
+	 * Registers event listeners.
+	 * @param plugin The plugin to register the events to.
+	 * @param listeners The listeners to register.
+	 */
+	
+	public static void registerEvents(Plugin plugin, Listener... listeners)
+	{
+		//For each listener register events
+		for (Listener listener : listeners)
+		{
+			Bukkit.getServer().getPluginManager().registerEvents(listener, plugin);
+		}
 	}
 }
